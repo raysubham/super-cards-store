ad571570801cf98a1db306141d06d77314e1c907
Subham Ray <self.programmed@gmail.com>
2021-09-23 03:13:09 +0530

    added individual product page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Script from 'next/script'
+import Link from 'next/link'
 
 import styles from '../styles/Home.module.scss'
 import { Header } from '../components/Header'
@@ -15,12 +15,6 @@ const Home: NextPage = () => {
       <Head>
         <title>Collectibles Store</title>
         <meta name='description' content='Find limited edition cards here.' />
-        <link rel='preconnect' href='https://app.snipcart.com' />
-        <link rel='preconnect' href='https://cdn.snipcart.com' />
-        <link
-          rel='stylesheet'
-          href='https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.css'
-        />
       </Head>
       <Header />
 
@@ -33,14 +27,20 @@ const Home: NextPage = () => {
               {products.map((product) => {
                 return (
                   <li key={product.id}>
-                    <Image
-                      width={864}
-                      height={1200}
-                      src={product.image}
-                      alt={`Card of ${product.title}`}
-                    />
-                    <h3 className={styles.productTitle}>{product.title}</h3>
-                    <p className={styles.productPrice}>Rs {product.price}</p>
+                    <Link href={`/products/${product.id}`}>
+                      <a>
+                        <Image
+                          width={864}
+                          height={1200}
+                          src={product.image}
+                          alt={`Card of ${product.title}`}
+                        />
+                        <h3 className={styles.productTitle}>{product.title}</h3>
+                        <p className={styles.productPrice}>
+                          Rs {product.price}
+                        </p>
+                      </a>
+                    </Link>
                     <p>
                       <Button
                         className='snipcart-add-item'
@@ -64,17 +64,6 @@ const Home: NextPage = () => {
       <footer className={styles.footer}>
         &copy; My Store, {new Date().getFullYear()}
       </footer>
-
-      <Script
-        async
-        src='https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.js'
-      />
-      <div
-        hidden
-        id='snipcart'
-        data-api-key={`${process.env.NEXT_PUBLIC_SNIPCART_API_KEY}`}
-        data-currency='inr'
-      />
     </>
   )
 }
